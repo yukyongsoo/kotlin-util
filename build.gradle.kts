c17da9ab227c7fd0390bf1bca7f19a5861a093ff
ys.yuk <ys.yuk@bunjang.co.kr>
2025-10-30 00:40:40 +0900

    imp(all): dependency version up
    + kotlin 2.2.21
    + kotlinter 5.2.0
    + junit 6
    + target JVM 17
    + gradle 9.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    val kotlinVersion = "2.0.0"
+    val kotlinVersion = "2.2.21"
 
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
 
     idea
 
-    id("org.jmailen.kotlinter") version "4.4.1"
+    id("org.jmailen.kotlinter") version "5.2.0"
 }
 
 allprojects {
@@ -26,25 +28,27 @@ subprojects {
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-        testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+        testImplementation("org.junit.jupiter:junit-jupiter:6.0.0")
+        testImplementation("org.junit.platform:junit-platform-launcher:6.0.0")
     }
 
-    java.sourceCompatibility = JavaVersion.VERSION_1_8
+    java.sourceCompatibility = JavaVersion.VERSION_17
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
+        compilerOptions {
+            freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 
     tasks.withType<Wrapper> {
-        gradleVersion = "8.3"
+        gradleVersion = "9.1.0"
     }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.0.0")
+    testImplementation("org.junit.platform:junit-platform-launcher:6.0.0")
 }
