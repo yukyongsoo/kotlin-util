381d804224ced0711d8f7d27b9787fcf5e5ecc9c
ys.yuk <ys.yuk@bunjang.co.kr>
2025-10-30 01:10:56 +0900

    chore(address): dependency version up
    + spring-webflux:6.2.12
    + reactor-netty-http:1.2.11
    + jackson-module-kotlin:2.20.0

--- address/build.gradle.kts
+++ address/build.gradle.kts
@@ -6,11 +6,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework:spring-webflux:5.3.9")
-    implementation("io.projectreactor.netty:reactor-netty-http:1.0.10")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("org.springframework:spring-webflux:6.2.12")
+    implementation("io.projectreactor.netty:reactor-netty-http:1.2.11")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.0")
 
-    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.7")
 }
 
 tasks.withType<Test> {
